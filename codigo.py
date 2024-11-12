6a884028b24fd8c3abcadc328fca9afc9e07c497
Diego <diegoabva2003@gmail.com>
2024-11-12 16:32:42 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -35,13 +35,13 @@ if opcion == 'Información':
     st.markdown("<h1 style='color: white;'>Información</h1>", unsafe_allow_html=True)
 
     st.markdown("<h3 style='color: white;'>¿Qué es League of Legends?</h3>", unsafe_allow_html=True)
-
     st.markdown("""
     <div style="text-align: justify; text-justify: inter-word;">
         <p style='color: white;'>League of Legends es un juego de estrategia por equipos en el que dos equipos conformados por cinco poderosos campeones se enfrentan para destruir la base del otro. Elige de entre más de 140 campeones para realizar jugadas épicas, asegurar asesinatos y destruir torretas mientras avanzas hacia la victoria.</p>
     </div>
     """, unsafe_allow_html=True)
-
+    st.image('imagenes/informacionuno.png', use_column_width=True)
+    
     st.markdown("<h3 style='color: white;'>Destruye el nexo del enemigo</h3>", unsafe_allow_html=True)
     
     st.markdown("""
