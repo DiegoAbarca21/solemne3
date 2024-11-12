a6281ed8e4cc48ad275fba4255d95b404f150278
Diego <diegoabva2003@gmail.com>
2024-11-12 16:19:17 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -31,13 +31,17 @@ st.image('imagenes/letras.png', use_column_width=True)
 opcion = st.sidebar.selectbox('Selecciona una sección', ['Información', 'Campeones', 'Competitivo', 'Acerca de'])
 
 if opcion == 'Información':
-    st.markdown("<h1 style='color: white;'>¿Qué es League of Legends?</h1>", unsafe_allow_html=True)
+
+    st.markdown("<h1 style='color: white;'>¿Información?</h1>", unsafe_allow_html=True)
+    
+    st.markdown("<h3 style='color: white;'>¿Qué es League of Legends?</h3>", unsafe_allow_html=True)
+    
     st.markdown("""
     <div style="text-align: justify; text-justify: inter-word;">
     <p style='color: white;'>League of Legends es un juego de estrategia por equipos en el que dos equipos conformados por cinco poderosos campeones se enfrentan para destruir la base del otro. Elige de entre más de 140 campeones para realizar jugadas épicas, asegurar asesinatos y destruir torretas mientras avanzas hacia la victoria.</p>
     """, unsafe_allow_html=True)
 
-    st.markdown("<h1 style='color: white;'>Destruye el nexo del enemigo</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='color: white;'>Destruye el nexo del enemigo</h3>", unsafe_allow_html=True)
     st.markdown("""
     <div style="text-align: justify; text-justify: inter-word;">
     <p style='color: white;'>El nexo es el corazón de las bases de ambos equipos. Destruye el nexo enemigo para ganar la partida.</p>
