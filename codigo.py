a7937cfb9a57ee475252fee724cf336551f8f8a5
Diego <diegoabva2003@gmail.com>
2024-11-12 21:08:03 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,179 @@
--pip install matplotlib
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import altair as alt
+import base64
+
+#base de datos pandas
+dfinf = pd.read_csv("backloggd_games.csv")
+
+def convertir_k(valor):
+    if isinstance(valor, str) and "K" in valor:
+        return float(valor.replace("K", "")) * 1000
+    else:
+        return float(valor)
+        
+dfinf["Playing"] = dfinf["Playing"].apply(convertir_k)
+
+#fondo
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+bg_image_path = "imagenes/fondo.jpg"
+bg_image_base64 = get_base64_of_bin_file(bg_image_path)
+
+page_bg_img = f"""
+<style>
+    .stApp {{
+        background-image: url("data:image/jpg;base64,{bg_image_base64}");
+        background-size: cover;
+        background-position: center;  /* Centra la imagen */
+    }}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+st.sidebar.image('imagenes/logo.png', use_column_width=True)
+st.image('imagenes/letras.png', use_column_width=True)
+
+opcion = st.sidebar.selectbox('Selecciona una sección', ['Información', 'Campeones', 'Competitivo', 'Acerca de'])
+
+if opcion == 'Información':
+    titulos = np.array(dfinf["Title"])
+    jugando = np.array(dfinf["Playing"])
+    
+    #TITULO
+    st.markdown(
+    """
+    <div style="text-align: center;">
+        <h1 style="color: white;">Información</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    
+    #QUE ES LEAGUE OF LEGENDS?
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">¿Qué es League of Legends?</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>League of Legends es un juego de estrategia por equipos en el que dos equipos conformados por cinco poderosos campeones se enfrentan para destruir la base del otro. Elige de entre más de 140 campeones para realizar jugadas épicas, asegurar asesinatos y destruir torretas mientras avanzas hacia la victoria.</p>
+    </div>
+    """, unsafe_allow_html=True)
+    st.image('imagenes/informacionuno.jpg', use_column_width=True)
+    
+    #DESTRUYE EL NEXO DEL ENEMIGO
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">Destruye el nexo del enemigo</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>El nexo es el corazón de las bases de ambos equipos los cuales no permitiran tan facil que los destruyas. Abrete paso entre los enemigos y destruye su nexo ganar la partida.</p>
+    </div>
+    """, unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("imagenes/nexoazul.jpg", caption="Los súbditos se generan en tu nexo. Detrás del nexo se encuentra la fuente, donde podrás recuperar vida y maná con rapidez y acceder a la tienda.")
+    with col2:
+        st.image("imagenes/nexorojo.jpg", caption="El nexo enemigo, que se encuentra en la base del equipo contrario, es igual que el de tu equipo. Si acabas con él, ganarás la partida.")
+        
+    #Abrete paso
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">Abrete paso</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Para llegar hasta el nexo enemigo, tu equipo tendrá que avanzar por al menos una calle. Hay estructuras defensivas que bloquean tu avance: las torretas y los inhibidores. Cada calle cuenta con tres torretas y un inhibidor. Además, cada nexo tiene dos torretas adicionales.</p>
+    </div>
+    """, unsafe_allow_html=True)
+    st.image('imagenes/torretainhibidores.jpg', use_column_width=True)
+    
+    #USA TUS HABILIDADES
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">Usa tus habilidades</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Los campeones cuentan con cinco habilidades básicas y dos hechizos especiales, y pueden equiparse con un máximo de siete objetos. Para que tu equipo se alce con la victoria, tendrás que ir descubriendo cuál es el orden de habilidades, hechizos de invocador y la configuración de objetos óptimos para tu campeón.</p>
+    </div>
+    """, unsafe_allow_html=True)
+    st.image('imagenes/habilidadescast.gif',caption="Personaje disparando la primera habilidad", use_column_width=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("imagenes/eclipseinf.jpg", caption="Objeto ideal para comprar cuando eres luchador, te otorga escudo, daño a enemigos con mucha vida y reestablecimiento de habilidades.")
+    with col2:
+        st.image("imagenes/ludencompanioninf.jpg", caption="Objeto ideal para comprar cuando eres mago, te otorga daño magico dispersivo, maná y reestablecimiento de habilidades.")
+        
+    # VIDEO JUEGO CON MAS JUGADORES
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">De lo mas jugado...</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>League of legends es un videojuego muy popular entre la cultura del videojuego, representado en la siguiente grafica sobre como ha sido uno de los mas jugados atraves del tiempo.</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+    plt.figure(figsize=(10, 5))
+    plt.plot(titulos, jugando, marker='o', color='b')
+    plt.title("Número de Jugadores por Título")
+    plt.xlabel("Título")
+    plt.ylabel("Jugando")
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+    
+elif opcion == 'Campeones':
+    st.write('Aquí van los datos.')
+    
+elif opcion == 'Competitivo':
+    st.markdown("<h1 style='color: white;'>Competitivo</h1>", unsafe_allow_html=True)
+    video_url = "https://www.youtube.com/watch?v=Kv2rswmxBVs"
+    st.video(video_url)
+    
+elif opcion == 'Acerca de':
+    st.write('Aquí se mostraría la información adicional.')
+
+
 
 
 
