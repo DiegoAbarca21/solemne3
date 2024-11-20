98a423c64546a6bf2442053ec94c047293be8caf
Diego <diegoabva2003@gmail.com>
2024-11-19 22:22:59 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -337,11 +337,13 @@ elif opcion == 'Campeones':
         """,
         unsafe_allow_html=True
         )
+            
         col1, col2 = st.columns(2)
         with col1:
                 st.image("imagenes/aatrox.jpg")
         with col2:
                 st.write("Aqui va la informacion")
+                
 
 elif opcion == 'Competitivo':
     st.markdown("<h1 style='color: white;'>Competitivo</h1>", unsafe_allow_html=True)
